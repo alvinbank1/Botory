9efbbc05fe7dbbd940005800d0a98d19eef2f44b
Undecember <ehdtjskaqhd@gmail.com>
2021-06-08 09:32:43 +0900

    Temporary commit

--- cogs/Moderator.py
+++ cogs/Moderator.py
@@ -1,17 +1,30 @@
 import discord, uuid, re
 from discord.ext import commands
-from StudioBot.pkgs.DBCog import DBCog
-from typing import union
+from pkgs.DBCog import DBCog
 
 class Core(DBCog):
     def __init__(self, app):
         self.CogName = 'Moderator'
         DBCog.__init__(self, app)
 
-    def initDB(self): return
+    def initDB(self):
+        self.DB = dict()
 
     @commands.command(name = 'ban')
-    async def ModBan(self, ctx, who: discord.User, reason = None):
+    @commands.has_guild_permissions(administrator = True)
+    async def ModBan(self, ctx, who, reason = None):
+        who = self.mention2member(who, ctx.guild)
         await ctx.guild.ban(who, reason = reason, delete_message_days = 7)
         embed = discord.Embed(title = 'RIP :zany_face:', description = f'**{who.name}#{who.discriminator}**')
         await ctx.send(embed = embed)
+
+    @commands.command(name = 'countban')
+    @commands.has_guild_permissions(administrator = True)
+    async def ModBan(self, ctx, count: int):
+        await ctx.message.delete()
+        msgs = await ctx.channel.history(limit = count).flatten()
+        for msg in msgs:
+            who = msg.author
+            if ctx.guild.get_member(who.id): continue
+            print(who.name)
+            #await ctx.guild.ban(who, delete_message_days = 7)
